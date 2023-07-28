370bc0495e19397f12ec98e7fe5c95d496f915ce
Setu Raj <seturaj110@gmail.com>
2023-07-28 23:09:01 +0530

    Daily Practice

---
+++ Recursion/Stairs_ways.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair.
+    int countWays(int n)
+    {
+        if(n<0) return 0;
+        if(n==0) return 1;
+        
+        return countWays(n-1) + countWays(n-2);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+
+// } Driver Code Ends
