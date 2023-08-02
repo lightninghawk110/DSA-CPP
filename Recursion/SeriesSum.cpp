3aee5b047c4261f362d1c3d336e3ba8e24234e59
Setu Raj <seturaj110@gmail.com>
2023-08-02 23:40:53 +0530

    Daily Practice

---
+++ Recursion/SeriesSum.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findNthTerm(int N) {
+    if ( N == 0 )
+        return 0;
+    int ans;
+    ans = N*(N+1)/2;
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findNthTerm(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
