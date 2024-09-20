38629ec60c2431882d571a91151a7ff2321c1f2b
Setu Raj <seturaj110@gmail.com>
2024-09-21 01:05:19 +0530

    grind++

--- Codeforces/cypher.cpp
+++ Codeforces/cypher.cpp
@@ -1,10 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void cypher(){
-    short n;
-    short Earr[n];
-    short Rarr[n];
-    short n_mov;
+short *cypher(short n)
+{
+    static short Earr[100]; // Use static array to avoid returning a local variable.
+
     for (short i = 0; i < n; i++)
     {
         scanf("%hd", &Earr[i]);
@@ -12,37 +11,56 @@ void cypher(){
 
     for (short i = 0; i < n; i++)
     {
-        
+        short n_mov;
         scanf("%hd", &n_mov);
-        char mov[n_mov];
-        for (short i = 0; i < n_mov; i++)
+        char mov[n_mov + 1]; // Add extra space for null character
+        scanf("%s", mov);    // read the movement sequence
+        for (short j = 0; j < n_mov; j++)
         {
-            scanf("%c", mov+1);
-        }
-         for (short i = 0; i < n_mov; i++)
-        {
-             if (mov[i] == 'D')
-        {
-            if (Earr[i] == 9)
+            if (mov[j] == 'D')
             {
-                Earr[i] = 0;
-            }else {
-                Earr[i] = Earr[i]++;
+                if (Earr[i] == 9)
+                {
+                    Earr[i] = 0;
+                }
+                else
+                {
+                    Earr[i]++;
+                }
             }
-            
-        } else{
-             if (Earr[i] == 0)
+            else if (mov[j] == 'U')
             {
-                Earr[i] = 9;
-            }else {
-                Earr[i] = Earr[i]--;
+                if (Earr[i] == 0)
+                {
+                    Earr[i] = 9;
+                }
+                else
+                {
+                    Earr[i]--;
+                }
             }
         }
-        }
-      
-        
-
     }
-    
-    
+
+    return Earr;
+}
+
+int main(int argc, char const *argv[])
+{
+    short t;
+    scanf("%hd", &t);
+
+    while (t--)
+    {
+        short *ptr;
+        short n;
+        scanf("%hd", &n);
+        ptr = cypher(n);
+        for (short i = 0; i < n; i++)
+        {
+            printf("%hd ", *(ptr + i));
+        }
+        printf("\n");
+    }
+    return 0;
 }
